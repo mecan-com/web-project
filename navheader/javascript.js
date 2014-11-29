d8cce9c122aba937f30e79ce3aa645573e0ba70b
MECAN <sinagolchin3046@gmail.com>
2014-11-28 23:44:27 -0500

    Small touch-ups on final nav look

--- navheader/javascript.js
+++ navheader/javascript.js
@@ -1,18 +1,14 @@
 $(function() {
 
-	$(window).scroll(function() {
-		var top_offset = $('body').offset().top;
-	  if (top_offset >= 40) {
-	            alert('yolo');
-	        } else {
-	            $('.main-nav').removeClass('main-nav-shadow');
-	        }
-	});
+
 
 	$(window).scroll(function(){console.log($(window).scrollTop());
 			if($(window).scrollTop() > 2)
 			{
-				$('.main-nav').addClass('main-nav-shadow')
+				$('.main-nav').addClass('main-nav-shadow');
+			}
+			else {
+				$('.main-nav').removeClass('main-nav-shadow');
 			}
 	 });
 
